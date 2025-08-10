7c2e5358440539098335d13e6fd40f4bdff8f635
Daniel Roke <droque123@gmail.com>
2025-08-10 23:15:51 +0300

    Simplify `react-native.config.js` by removing unused Android configuration and streamlining assets setup.

--- react-native.config.js
+++ react-native.config.js
@@ -1,13 +1,9 @@
-// react-native.config.js
 module.exports = {
     dependency: {
         platforms: {
-            android: {
-                packageImportPath: 'import com.rngodotview.GodotBridgePackage;',
-                packageInstance: 'new GodotBridgePackage()',
-            },
-            ios: {},
+            android: {},
+            ios: {}
         },
-        assets: [], // no publicamos assets desde el paquete; el .pck vive en el proyecto del usuario
-    },
+        assets: []
+    }
 };
